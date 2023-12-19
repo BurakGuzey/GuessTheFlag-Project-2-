c0fb1c72c205eea3883808d3114635b17d915e5e
Burak Güzey <burakguzey20@gmail.com>
2023-12-19 21:22:52 +0300

    Setting up and designing the layout

---
+++ Guess The Flag - Project 2/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Guess The Flag - Project 2
+//
+//  Created by Burak Güzey on 19.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
